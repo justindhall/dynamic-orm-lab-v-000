539a991bc419cb414ca9e5a8066c13a53119c0ab
Justin Hall <hall.justin.d@gmail.com>
2019-04-22 20:54:14 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,5 +7,19 @@ class InteractiveRecord
   end
 
   def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = <<-SQL
+      PRAGMA table_info('#{table_name}')
+    SQL
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+
+    column_names.compact
   end
 end
