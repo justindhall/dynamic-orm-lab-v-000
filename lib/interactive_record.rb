c4f592d91a894cfca73f4e5dcdb3ef23ad5ac828
Justin Hall <hall.justin.d@gmail.com>
2019-04-22 21:14:38 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,15 +7,13 @@ class InteractiveRecord
   end
 
   def self.column_names
-    sql = <<-SQL
-      PRAGMA table_info('#{table_name}')
-    SQL
+
+    sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
-
-    table_info.each do |column|
-      column_names << column["name"]
+    table_info.each do |row|
+      column_names << row["name"]
     end
     column_names.compact
   end
@@ -26,12 +24,14 @@ class InteractiveRecord
     end
   end
 
-  def table_name_for_insert
-    self.class.table_name
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  def table_name_for_insert
+    self.class.table_name
   end
 
   def values_for_insert
@@ -42,25 +42,17 @@ class InteractiveRecord
     values.join(", ")
   end
 
-  def save
-    sql = <<-SQL
-      INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})
-    SQL
-
-    DB[:conn].execute(sql)
-
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT * FROM #{self.table_name} WHERE name = '#{name}'
-    SQL
-
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(value)
+  def self.find_by(hash)
+    value = hash.values.first
   end
 
 end
