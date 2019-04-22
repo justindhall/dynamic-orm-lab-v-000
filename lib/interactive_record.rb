f0d5b43c95651958358287689fe8c9e1611def1f
Justin Hall <hall.justin.d@gmail.com>
2019-04-22 20:58:51 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -29,4 +29,12 @@ class InteractiveRecord
     end
   end
 
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def column_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}
+  end
+
 end
